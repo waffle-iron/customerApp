bfebfa0f1709e5ad024aad7a0aefb71d9e4f23cd
aripal <dleroari@gmail.com>
2017-03-05 19:36:49 -0500

    Rechange infrastructure

---
+++ Project/js/app.js
@@ -0,0 +1,9 @@
+
+// user interface controller
+//var viewController = (function(){})();
+// modelController
+//var modelController = (function(){})();
+// Logical Controller
+//var controller = (function(viewCtrl, modelCtrl){})(viewController, modelController);
+
+alert('Javascript is working');
