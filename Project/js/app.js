01a887055d0aa06e2f55490bbb70ee4072dad8f4
aripal <dleroari@gmail.com>
2017-03-05 21:10:51 -0500

    Added Ajax server request

--- Project/js/app.js
+++ Project/js/app.js
@@ -1,9 +1,19 @@
 
-// user interface controller
-//var viewController = (function(){})();
-// modelController
-//var modelController = (function(){})();
-// Logical Controller
-//var controller = (function(viewCtrl, modelCtrl){})(viewController, modelController);
+// Save Data
+function saveData(){
+  var name = $('#inputName').val();
+  var plateNumber = $('#inputPlateNumber').val();
+  var price = $('#inputPrice').val();
+  var insideWash = $('#inWash').val();
+  var outsideWash = $('#outWash').val();
 
-// alert('Javascript is working');
+  // ajax send request
+  $.ajax({
+    type: "POST",
+    url: "service.php?p=add",
+    data: "inputName=" + name +"&inputPlateNumber=" + plateNumber + "inputPrice=" + price + "&inWash=" + inWash + "&outWash=" + outWash,
+    success: function(msg){
+      alert('Success: Insert data');
+    }
+  });
+}
